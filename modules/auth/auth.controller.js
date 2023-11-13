30436c38017b12e36c068978beb67d262fd790dd
mt_tahmid <md.tasin.tahmid1998@gmail.com>
2023-11-13 20:05:58 +0600

    third commit: register and login route complete

--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -3,14 +3,54 @@ const jwt = require("jsonwebtoken");
 const User = require("../user/user.model");
 
 const register = async(req, res) => {
-    // cosnt {}
+    try {
+        const { username, email, password } = req.body;
 
-    const newUser = await User.create(req.body);
-    res.status(200).send(newUser);
+        const emailExists = await User.findOne({
+            where: { email }
+        });
+
+        if(emailExists){
+            return res.status(400).send({"messege": "User already exists with this email."});
+        }
+
+        const salt = await bcrypt.genSalt();
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashPassword
+        });
+
+        res.status(201).send(newUser);
+    } catch (error) {
+        console.log(error.messege);
+        res.status(500).send({"messege": "Internal server error."});
+    }
 };
 
 const login = async(req, res) => {
-    res.status(200).send(req.body);
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({
+            where: { email }
+        });
+
+        if(!user) return res.status(404).send({"messege": "User not found."});
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).send({"messege": "Invalid credentials."});
+        
+        const token = jwt.sign({id: user.id}, "secret-key");
+
+        res.status(200).send({ token });
+
+    } catch (error) {
+        console.log(error.messege);
+        res.status(500).send({"messege": "Internal server error."});    
+    }
 };
 
 module.exports = { register, login };
--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("blog_server","root","password",{
+const sequelize = new Sequelize("blog_server","root","root",{
     dialect: "mysql",
     host: "localhost"
 });
 
-sequelize.sync({force: true})
+sequelize.sync()
     .then(()=>{console.log('connected successfully')})
     .catch(()=>{console.log('not conneted')});
 
