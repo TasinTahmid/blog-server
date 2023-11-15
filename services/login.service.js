c9c1f7f2504129264583e5a7951daca60675f35c
Md Tasin Tahmid <md.tasin.tahmid1998@gmail.com>
2023-11-15 16:50:16 +0600

    sixth commit: login service and repository done

--- services/login.service.js
+++ services/login.service.js
@@ -1,9 +1,12 @@
 const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { findOneUser } = require('../repositories/user.repository');
 
 
 const loginService = async(email, password) => {
     try {
-        const user = await User.findOne({ where: { email } });
+        const user = await findOneUser(email);
 
         if(!user) return res.status(404).send({"messege": "User not found."});
 
@@ -13,7 +16,7 @@ const loginService = async(email, password) => {
         return jwt.sign({id: user.id}, "secret-key");
 
     } catch (error) {
-        console.log(error.messege);
+        console.log(error);
         res.status(500).send({"messege": "Internal server error."});    
     }
 };
